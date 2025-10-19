cf829e3428344785daf1267834d20505ca47d76f
yorozc <yorozcolop49@gmail.com>
2025-10-19 16:32:50 -0700

    added needed imports

--- website/auth.py
+++ website/auth.py
@@ -1,3 +1,7 @@
 from flask import render_template, redirect, Blueprint, url_for, flash, request, session
+from flask_login import login_user, login_required, logout_user, current_user
+from werkzeug.security import generate_password_hash, check_password_hash 
+from .User import User 
+from.db import users 
 
 auth = Blueprint('auth', __name__)
--- website/routes.py
+++ website/routes.py
@@ -1,4 +1,6 @@
 from flask import render_template, redirect, Blueprint, url_for, flash, request, session
+from flask_login import login_required, current_user
+from .db import users
 
 routes = Blueprint('routes', __name__)
 
