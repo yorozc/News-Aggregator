616ce04326d1fa4d2024e6cc752a3b28786796ec
yorozc <yorozcolop49@gmail.com>
2025-11-12 10:37:26 -0800

    added fieldset to login form and basic styling

--- website/rss_feeds.py
+++ website/rss_feeds.py
@@ -7,12 +7,6 @@ rss_feeds = {
     'CNBC': 'https://search.cnbc.com/rs/search/combinedcms/view.xml?partnerId=wrss01&id=15839069'
 }
 
-'''
-list of news stations to add
-Hacker News, 
-https://news.ycombinator.com/item?id=16908241
-'''
-
 def parse():
     articles = []
     for source, feed in rss_feeds.items():
@@ -23,7 +17,5 @@ def parse():
     # sorts articles for the newest
     articles = sorted(articles, key=lambda x: x[1].published_parsed, reverse=True)
 
+    print(articles[1][1])
     return articles
-
-# parse can be used for the daily debrief (most recent news)
-# then I can jsut randomize the rest to display them until I think of an algo to control the displaying of articles
