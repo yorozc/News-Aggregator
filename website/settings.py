d1489c82317c6d77c577ff9e6b7cda9042a62737
yorozc <yorozcolop49@gmail.com>
2025-11-27 19:11:46 -0800

    added more to settings page

--- website/settings.py
+++ website/settings.py
@@ -1,11 +1,18 @@
+from collections import UserDict
+from bson import ObjectId
 from flask import render_template, redirect, Blueprint, url_for, flash, request, jsonify, session
 from flask_login import login_required, current_user
+from werkzeug.security import check_password_hash
+from .db import get_users_collection
 
 settings = Blueprint('settings', __name__)
 
 @login_required
 @settings.route("/settings", methods=["GET", "POST"])
 def user_settings():
+    users = get_users_collection()
+    user_id = ObjectId(current_user.id)
+    user = users.find_one({'_id': user_id})
 
-    return render_template("settings.html", name=current_user.username)
+    return render_template("settings.html", name=current_user.username, email=user["email"])
 
